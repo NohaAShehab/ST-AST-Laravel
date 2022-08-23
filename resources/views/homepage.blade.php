8baba1dc0ae79663ab8b362245f5a204d132004d
Noha Shehab <nohashehab.iti@gmail.com>
2022-08-23 12:37:19 +0200

    template inheritance

--- resources/views/homepage.blade.php
+++ resources/views/homepage.blade.php
@@ -1,39 +1,14 @@
-<!DOCTYPE html>
-<html lang="en">
+@extends('layouts.app')
 
-<head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <meta http-equiv="X-UA-Compatible" content="ie=edge">
-    <title>Document</title>
-    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.0/dist/css/bootstrap.min.css" rel="stylesheet"
-        integrity="sha384-gH2yIJqKdNHPEq0n4Mqa/HGKIhSkIHeL5AyhkYV8i59U5AR6csBvApHHNl/vI1Bx" crossorigin="anonymous">
-    <style>
-        body {
-            font-size: x-large;
-            font-family: 'Courier New', Courier, monospace;
-        }
-    </style>
-</head>
 
-<body>
-    <div class="container">
-        <h1> this my template </h1>
-        <h3 style="color: red"> {{$myuser}}</h3>
 
-            {{$num}}
-        {{-- {{$info}} --}}
+@section('main')
+    Nohaaaaaaaaaaaaaaaaaaaaaa
+@endsection
 
-        {{$info["name"]}} {{$info["dept"]}}
+
+{{-- @section('content')
+    <div>
+        <h1> This my home page {{ $myuser }} </h1>
     </div>
-
-
-    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.2.0/dist/js/bootstrap.bundle.min.js"
-        integrity="sha384-A3rJD856KowSb7dwlZdYEkO39Gagi7vIsF0jrRAoQmDKKtQBHUuLZ9AsSv4jD4Xa" crossorigin="anonymous">
-    </script>
-    {{-- <script>
-        alert("Good morning from laravel")
-    </script> --}}
-</body>
-
-</html>
+@endsection --}}
--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -1,39 +1,61 @@
 <!DOCTYPE html>
 <html lang="en">
+
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
     <title>Document</title>
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.0/dist/css/bootstrap.min.css" rel="stylesheet"
-    integrity="sha384-gH2yIJqKdNHPEq0n4Mqa/HGKIhSkIHeL5AyhkYV8i59U5AR6csBvApHHNl/vI1Bx" crossorigin="anonymous">
-<style>
-    body {
-        font-size: x-large;
-        font-family: 'Courier New', Courier, monospace;
-    }
-</style>
+        integrity="sha384-gH2yIJqKdNHPEq0n4Mqa/HGKIhSkIHeL5AyhkYV8i59U5AR6csBvApHHNl/vI1Bx" crossorigin="anonymous">
+    <style>
+        body {
+            font-size: x-large;
+        }
+    </style>
 </head>
 
 <body>
 
-<nav class="navbar navbar-expand-lg bg-light">
-    <div class="container-fluid">
-      <a class="navbar-brand" href="#">ITI Website</a>
-      <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
-        <span class="navbar-toggler-icon"></span>
-      </button>
-      <div class="collapse navbar-collapse" id="navbarSupportedContent">
-        <ul class="navbar-nav me-auto mb-2 mb-lg-0">
-          <li class="nav-item">
-            <a class="nav-link active" aria-current="page" href="/">Laravel Home</a>
-          </li>
+    <nav class="navbar navbar-expand-lg bg-light">
+        <div class="container-fluid">
+            <a class="navbar-brand" href="#">ITI Website</a>
+            <button class="navbar-toggler" type="button" data-bs-toggle="collapse"
+                data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false"
+                aria-label="Toggle navigation">
+                <span class="navbar-toggler-icon"></span>
+            </button>
+            <div class="collapse navbar-collapse" id="navbarSupportedContent">
+                <ul class="navbar-nav me-auto mb-2 mb-lg-0">
+                    <li class="nav-item">
+                        <a class="nav-link active" aria-current="page" href="/">Laravel Home</a>
+                    </li>
 
-      </div>
+                    <li class="nav-item">
+                        <a class="nav-link active" aria-current="page" href="/users">All users</a>
+                    </li>
+
+            </div>
+        </div>
+    </nav>
+
+
+    <div class="container " style="background-color:antiquewhite">
+        @yield('content')
     </div>
-  </nav>
+
+    <div class="container " style="background-color:yellow">
+        @yield('main')
+    </div>
 
 
-  
+
+
+
+    <footer>
+        <h3 style="text-align: center">copyrights@iti2022 </h3>
+    </footer>
+
 </body>
+
 </html>
